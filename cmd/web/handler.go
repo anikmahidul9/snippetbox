126efdf90f4a8677eccd2606d743db2fc91371ee
anikmahidul9 <mahidulanik9@gmail.com>
2026-05-27 02:38:00 +0600

    frontend-setup and file server add

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from Snippetbox"))
+	//Use the template.ParseFiles() function to read  the template file into a template set http.Error() function to send an internal server error response to the user, and then return from the handler so no subsequent code is executed.
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil{
+		log.Println(err.Error())
+		http.Error(w,"Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	//Then we use the Execute() method on the template set to write the template content as the response body. the last parameter to execute is the dynamic data that we want to pass in to the template. In this case we don't have any dynamic data to pass in, so we use nil.
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil{
+		log.Println(err.Error())
+		http.Error(w,"Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	//Create a file server which serves files out of the "./ui/static" directory. The path given to the http.dir function is relative to the project root directory.
+
+fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	//Use the mux.Handle() method to register the file server as the handler for all URL paths that start with "/static/". We use the http.StripPrefix() function to create a new handler which will remove the "/static" prefix from the request URL's path before passing the request to the file server. This is necessary because our static files are located in the "./ui/static" directory, but we want them to be accessible via URLs that start with "/static/".
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{$}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
